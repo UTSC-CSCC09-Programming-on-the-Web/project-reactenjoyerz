93243a1fcbeeacc2b7dc8b3e932565d24a308bd5
Jason Yee <jasongahjun.yee@mail.utoronto.ca>
2025-06-17 15:21:51 -0400

    prettify + add background for game board

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
 import { Routes } from '@angular/router';
+import { GameBoard } from './game-board/game-board';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: '', component: GameBoard }];
---
+++ src/app/game-board/game-board.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-game-board',
+  imports: [],
+  templateUrl: './game-board.html',
+  styleUrl: './game-board.css',
+})
+export class GameBoard {}
