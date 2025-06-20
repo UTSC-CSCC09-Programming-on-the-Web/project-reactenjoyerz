bbffb030000138688882c9fdb9e5ceca224ceab7
YMichaelHao <michael4hao@gmail.com>
2025-06-20 11:29:53 -0400

    simple frontend flow for login & register

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { GameBoard } from './game-board/game-board';
 import { Home } from './home/home'
+import { Login } from './login/login'
+import { Register } from './register/register'
 export const routes: Routes = [
     { path: 'game', component: GameBoard },
     { path: 'home', component: Home },
+    { path: 'login', component: Login },
+    { path: 'register', component: Register },
     { path: '**', redirectTo:'home' }
 ];
