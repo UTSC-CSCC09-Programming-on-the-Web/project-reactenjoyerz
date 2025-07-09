b1082507ff7da9d26604266321dddfc4253b1f82
Jason Yee <jasongahjun.yee@mail.utoronto.ca>
2025-07-09 00:02:19 -0400

    prevent users from joining non existant games

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -7,9 +7,10 @@ import { Register } from './register/register'
 import { Subscribe } from './subscribe/subscribe';
 import { AuthGuard } from './services/auth.guard';
 import { GoogleLogin } from './google-login/google-login';
+import { GameGuard } from './services/game.guard';
 
 export const routes: Routes = [
-    { path: 'game', component: GameBoard, canActivate: [AuthGuard] },
+    { path: 'game', component: GameBoard, canActivate: [AuthGuard, GameGuard] },
     { path: 'match', component: MatchQueue, canActivate: [AuthGuard] },
     { path: 'home', component: Home },
     { path: 'login', component: Login },
